63ba3a6959edaf0f37dd76f4c3e7c5484805c326
krudos <kennethrc@gmail.com>
2016-06-12 23:49:18 -0600

    initial

---
+++ iosInvokeExampleA/iosInvokeExampleA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosInvokeExampleA
+//
+//  Created by krudos on 6/12/16.
+//  Copyright © 2016 krudos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosInvokeExampleB/iosInvokeExampleB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosInvokeExampleB
+//
+//  Created by krudos on 6/12/16.
+//  Copyright © 2016 krudos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
